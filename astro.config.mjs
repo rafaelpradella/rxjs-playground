13b966b855c5eb844a5c22caf80bd993dc42c30d
rafaelpradella <pradella@outlook.com>
2024-01-10 10:34:03 +0000

    Adding netlify ssr plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import solidJs from "@astrojs/solid-js";
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
   vite: {
     css: {
-      transformer: 'lightningcss',
+      transformer: 'lightningcss'
     },
     build: {
       cssMinify: 'lightningcss'
     }
-  }
+  },
+  output: "server",
+  adapter: netlify()
 });
